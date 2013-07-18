d0856c2a17c98e8817495a4d29ee06fb4fedd26b
John Wood <j@jdtw.us>
2013-07-17 23:35:29 -0700

    Removed duplicated code

--- bloggen.lisp
+++ bloggen.lisp
@@ -4,14 +4,4 @@
 
 ;;; "bloggen" goes here. Hacks and glory await!
 
-(defun markdown-p (path)
-  (let ((ext (getf (file-ext (file-ext path)) :ext)))
-    (or (equalp ext "md")
-        (equalp ext "markdown")
-        (equalp ext "text"))))
 
-(defun file-ext (path)
-  (let ((result (nth-value 1 (scan-to-strings
-                              "(^[\\w\\.]+)\\.([^\\.]+$)"
-                              (file-namestring path)))))
-    (list :file (aref result 0) :ext (aref result 1))))
